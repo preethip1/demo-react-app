1abdf5623e592be7bfb5e1d8c2dc387dc9ae2dab
Preethi <preethiknot1998@gmail.com>
2021-09-06 12:33:25 +0530

    added delete and reset button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state={ 
-        value: this.props.value,
-        tags: ["tag1" , "tag2" , "tag3"]
-    };
-    product = 0
     // constructor() {
     //     // first we have to call the constructor which is the parent(base) class that's why we are using the super() 
     //     super();
@@ -13,45 +8,54 @@ class Counter extends Component {
     //     this.handleIncrement = this.handleIncrement.bind(this);
     // }
 
-    renderTags() {
-        if(this.state.tags.length === 0) return <p> There are no tags!</p>;
+    // renderTags() {
+    //     if(this.state.tags.length === 0) return <p> There are no tags!</p>;
 
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    //     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
             
-    }
+    // }
 
-    handleIncrement = product => {
-        this.setState({count: this.state.value + 1});
-        console.log(product);
-    }
+    // handleIncrement = product => {
+    //     this.setState({value: this.state.value + 1});
+    // }
+
+    
     
     // styles = {
     //     fontSize : 15,
     //     fontWeight: 'bold'
     // }
     render() {
-        console.log('props',this.props) 
         return (
         <div>
             {/* {this.props.children} */}
             <h4>{this.props.children}</h4>
+            <button 
+            onClick={this.handleReset}
+            className = "btn btn-primary btn-sm m-2"
+            >Reset</button>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button onClick={(product) => this.handleIncrement(product)} className="btn btn-seecondary btn-sm">Increment</button>
-            {this.state.tags.length === 0 && 'Please create a new tag!'}
-            {this.renderTags()}
+            <button 
+                onClick={() => this.props.onIncrement(this.props.counter)} 
+                className="btn btn-seecondary btn-sm">Increment</button>
+            <button 
+                onClick={() => this.props.onDelete(this.props.counter.id)} 
+                className = "btn btn-danger btn-sm m-2">Delete</button>
+            {/* {this.state.tags.length === 0 && 'Please create a new tag!'} */}
+            {/* {this.renderTags()} */}
         </div>
         );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
      formatCount() {
-         const {value: count} = this.state;
-      return count === 0 ? 'Zero' : count;
+         const {value} = this.props.counter;
+      return value === 0 ? 'Zero' : value;
 
     }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,10 +10,34 @@ class Counters  extends Component {
             {id:4, value:0}
         ]
      }
+
+     handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter)
+        counters[index] = {...counters}
+        counters[index].value ++;
+        this.setState({counters})
+    }
+
+     handleDelete = counterId => {
+         const counters = this.state.counters.filter(c =>  c.id !== counterId);
+         this.setState({counters:counters})
+     }
+     handleReset = () => {
+        this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        })
+    }
     render() { 
         return (<div>    
             {this.state.counters.map(counter => 
-            <Counter key={counter.id} value={counter.value}>
+            <Counter 
+                key={counter.id} 
+                onDelete = {this.handleDelete}
+                onIncrement = {this.handleIncrement}
+                counter = {counter}
+                >
                 <h4>Counter{counter.id}</h4>
             </Counter>
                 )}   
